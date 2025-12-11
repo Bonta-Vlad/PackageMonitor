a8b29861c92ddeec2b9442773b4e83c05402e46d
SecuiuRaulS <secuiuraul@gmail.com>
2025-12-11 15:56:52 +0200

    gitignore and monitor

---
+++ monitor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+L_FILE="/var/log/dpkg.log"
+awk '/remove | installed/ {split($5,a,":"); print a[1]}' $L_FILE | sort -u | xargs -I % mkdir -p Packages/%
+for i in `ls Packages`
+do
+    awk -v name="$i" '/(remove|installed|half-installed)*name/ {print $0}' $L_FILE #> ./Packages/$i/ops.txt
+done
