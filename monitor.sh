af0162b4cf4b5d61a8a84c72e5f673969b51f3dd
SecuiuRaulS <secuiuraul@gmail.com>
2025-12-11 16:48:07 +0200

    fixed file writing

--- monitor.sh
+++ monitor.sh
@@ -4,5 +4,5 @@ L_FILE="/var/log/dpkg.log"
 awk '/remove | installed/ {split($5,a,":"); print a[1]}' $L_FILE | sort -u | xargs -I % mkdir -p Packages/%
 for i in `ls Packages`
 do
-    awk -v name="$i" '/(remove|installed|half-installed)*name/ {print $0}' $L_FILE #> ./Packages/$i/ops.txt
+    awk -v name="$i" '$0 ~ name && /(remove|installed)/' "$L_FILE" > "./Packages/$i/ops.txt"
 done
